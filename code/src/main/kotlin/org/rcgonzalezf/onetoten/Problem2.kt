72da39e58787dd3bed01983825fd4ef20e61c541
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2017-08-12 04:12:20 -0700

    Simplification of the Sequence.

--- code/src/main/kotlin/org/rcgonzalezf/onetoten/Problem2.kt
+++ code/src/main/kotlin/org/rcgonzalezf/onetoten/Problem2.kt
@@ -21,8 +21,7 @@ class Problem2 {
         val max = BigInteger.valueOf(maxValue.toLong())
 
         return fibonacciValues
-                .filter { filterCriteria(it.second, max) }
-                .map { it.second }
+                .filter { filterCriteria(it, max) }
                 .fold(BigInteger.ZERO, BigInteger::add)
     }
 
@@ -35,11 +34,10 @@ class Problem2 {
     }
 
     // based on https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.sequences/generate-sequence.html
-    fun fibonacci(): Sequence<Pair<BigInteger, BigInteger>> {
-        // fibonacci terms
-        // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, ...
-        return generateSequence(Pair(BigInteger.ZERO, BigInteger.ONE),
+    fun fibonacci(): Sequence<BigInteger> {
+        return generateSequence(Pair(BigInteger.ONE, BigInteger.ONE),
                 { Pair(it.second, it.first.add(it.second)) })
+                .map { it.first }
     }
 
 }
