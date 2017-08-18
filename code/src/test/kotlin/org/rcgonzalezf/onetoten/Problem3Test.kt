79555e36d69c22c3333db38435a5e0ef1a55959a
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2017-08-17 22:51:07 -0700

    Test for problem 3, it's ok if it fails we're showing how TDD helps us in the design of our code

---
+++ code/src/test/kotlin/org/rcgonzalezf/onetoten/Problem3Test.kt
@@ -0,0 +1,54 @@
+package org.rcgonzalezf.onetoten
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.*
+
+class Problem3Test {
+
+    private var uut: Problem3? = null
+    private var n: Long = 0
+    private var result: Long = 0
+
+    @Before
+    fun setupInstance() {
+        uut = Problem3()
+    }
+
+    @Test
+    fun shouldReturn29For13195() {
+        givenPrimeFactorOf(13185)
+
+        whenCalculatingSolution()
+
+        thenTheSolutionShouldBe(29)
+    }
+
+    @Test
+    fun shouldReturnSolutionFor600851475143() {
+        givenPrimeFactorOf(600851475143)
+
+        whenCalculatingSolution()
+
+        thenTheSolutionShouldBe(-1)
+    }
+
+    //region - given methods
+    private fun givenPrimeFactorOf(i: Long) {
+        this.n = i
+    }
+    //endregion
+
+    //region - when methods
+    private fun whenCalculatingSolution() {
+        result = uut.solve(n)
+    }
+    //endregion
+
+    //region - then methods
+    private fun thenTheSolutionShouldBe(expected: Int) {
+        assertEquals(expected, result)
+    }
+    //endregion
+
+}
