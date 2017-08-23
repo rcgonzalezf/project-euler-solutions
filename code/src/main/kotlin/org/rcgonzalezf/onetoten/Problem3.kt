6d84d697da3ffe5c2393dc3e3042a124b0f575b3
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2017-08-22 23:00:35 -0700

    Improvements into a more functional approach

--- code/src/main/kotlin/org/rcgonzalezf/onetoten/Problem3.kt
+++ code/src/main/kotlin/org/rcgonzalezf/onetoten/Problem3.kt
@@ -1,7 +1,5 @@
 package org.rcgonzalezf.onetoten
 
-import com.sun.org.apache.xpath.internal.operations.Number
-
 /**
 https://projecteuler.net/problem=3
 Largest prime factor Problem 3
@@ -18,31 +16,20 @@ class Problem3 {
     // 2. Sequence of prime factors, get the maximum
     // 3. The solution will be returned
     fun solve(n: Long): Long {
-        var sequenceOfPrimes = mutableListOf<Long>()
-        val sequenceOfFactors = generateSeqOfFactors(n)
+        val sequenceOfPrimes = mutableListOf<Long>()
 
-        sequenceOfFactors
-                .filter { isPrime(it) }
-                .forEach { sequenceOfPrimes.add(it) }
-
-        var max: Long = Long.MIN_VALUE
-
-        sequenceOfPrimes.forEach {
-            if (it > max)
-                max = it
-        }
-        return max
+        return generateSeqOfFactors(n)
+                .filterTo(sequenceOfPrimes)
+                { isPrime(it) }
+                .max() ?: -1L
     }
 
-
     private fun generateSeqOfFactors(n: Long): MutableList<Long> {
-        var sequence = mutableListOf<Long>()
-        val sqrt = Math.sqrt(n.toDouble()).toLong()
-        for (i in 2L until sqrt) { // TODO until sqrt of n
-            if (n.rem(i) == 0L) {
-                sequence.add(i)
-            }
-        }
+        val sequence = mutableListOf<Long>()
+        val limit = calculateLimit(n)
+        (2L until limit)
+                .filterTo(sequence)
+                { n.rem(it) == 0L }
         return sequence
     }
 
@@ -50,20 +37,16 @@ class Problem3 {
         if ((number % 2L) == 0L) {
             return number == 2L
         }
-        val sqrt = Math.sqrt(number.toDouble())
 
-        /*
-         return if ((3 until sqrt.toInt()).any { // TODO probably test with this auto-generated code
-            number % it == 0L
-        }) false else number != 1L
-         */
+        val limit = calculateLimit(number)
 
-        for (i in 3 until Math.ceil(sqrt).toLong()) { // TODO increment 2 by 2
-            if (number % i == 0L) {
-                return false
-            }
-        }
-        return number != 1L
+        return if ((3 until limit step 2)
+                .any { number % it == 0L }) false
+                else number != 1L
     }
 
+    private fun calculateLimit(number: Long): Long {
+        val sqrt = Math.sqrt(number.toDouble())
+        return Math.ceil(sqrt).toLong()
+    }
 }
